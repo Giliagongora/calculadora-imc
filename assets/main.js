0d3896432c34d510a44b5f827f653653681e48cd
Gilia Gongora <giliagongora.g@gmail.com>
2017-05-08 23:01:07 -0300

    desarrollo js

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,11 @@
 function calcularIMC(){
 	var peso = document.getElementById("peso").value;
 	var altura = document.getElementById("altura").value;
+	
+	if(peso == "" || altura == ""){
+		alert("Llena los campos vacios");
+	}else{
+		alert("Tu IMC es de: " + (parseInt(peso/Math.pow(altura,2))));
+	}
 
 }
