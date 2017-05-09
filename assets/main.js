29db8cf3e961381072717b77040c5678b2d0e8cc
Maribel Diaz <maribel.diaz.j@gmail.com>
2017-05-08 23:57:17 -0300

    calculando imc

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,18 @@
+
 function calcularIMC(){
+	
+	var peso = document.getElementById("peso").value;
+	var altura = document.getElementById("altura").value;
+
+	if(peso == "" ){
+		alert("Llena los campos vacios");
+else if(altura == ""){
+	alert("Llena los campos vacios");
+}
+}else{
+		alert("Tu IMC es de: " + (parseInt(peso/Math.pow(altura,2))));
+	}
 
 }
+
+
